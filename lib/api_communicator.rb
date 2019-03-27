b25554cc2bee782b1436bce5564c78bd40fec8b2
github username <github email address>
2019-03-27 10:39:39 -0400

    marked up api_communicator refactoring

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -7,6 +7,31 @@ require 'pry'
    response_hash = JSON.parse(response_string)
  end
 
+def compile_all_multiple_choice_answers(questions)
+  all_answers = questions["incorrect_answers"]
+  all_answers << questions["correct_answer"]
+  all_answers.shuffle!
+end
+
+def multiple_choice_hash_maker(compiled_answer)
+  answer_hash = Hash.new{0}
+  hash_counter = 1
+     compiled_answer.each do |answer|
+      answer_hash[hash_counter] = answer
+      hash_counter += 1
+      # binding.pry
+     end
+   answer_hash
+end
+
+def format_display_mult_choi_answers(compiled_answer_2)
+  counter = 1
+    compiled_answer_2.each do |answer|
+      puts "#{counter} - #{answer}"
+      counter += 1
+    end
+end
+
 def generate_questions(api_hash_of_questions)
   score_counter = 0
   question_counter = 1
@@ -19,33 +44,43 @@ def generate_questions(api_hash_of_questions)
     # puts question["incorrect_answers"]
       if question["type"] == 'multiple'
         puts "Please choose a number betwee 1 and 4"
-       all_answers = question["incorrect_answers"]
-       all_answers << question["correct_answer"]
-       all_answers.shuffle!
+# =======================
+        compiled_answers = compile_all_multiple_choice_answers(question)
+       # all_answers = question["incorrect_answers"]
+       # all_answers << question["correct_answer"]
+       # all_answers.shuffle!
 # binding.pry
-       answer_hash = Hash.new{0}
-       hash_counter = 1
-         all_answers.each do |answer|
-           answer_hash[hash_counter] = answer
-           hash_counter += 1
-           # binding.pry
-         end
+# =======================
+        mult_cho_hash = multiple_choice_hash_maker(compiled_answers)
+       # answer_hash = Hash.new{0}
+       # hash_counter = 1
+       #    compiled_answers.each do |answer|
+       #     answer_hash[hash_counter] = answer
+       #     hash_counter += 1
+       #     # binding.pry
+       #  end
+# =======================
+
        # puts answer_hash
-
-       counter = 1
-         all_answers.each do |answer|
-           puts "#{counter} - #{answer}"
-           # answer_hash = {"#{counter}" => "#{answer}"}
-            # if answer_hash
-           counter += 1
-         end
-
+# =======================
+      format_display_mult_choi_answers(compiled_answers)
+#        counter = 1
+#          compiled_answers.each do |answer|
+#            puts "#{counter} - #{answer}"
+#            # answer_hash = {"#{counter}" => "#{answer}"}
+#             # if answer_hash
+#            counter += 1
+# # =======================
+#          end
        answer = gets.chomp
-          if answer_hash[answer.to_i] == question["correct_answer"]
+
+       if mult_cho_hash[answer.to_i] == question["correct_answer"]
+          # if answer_hash[answer.to_i] == question["correct_answer"]
             puts "Correct!"
             puts " "
             score_counter += 1
           else
+            # binding.pry
             puts "You wrong!"
             puts "It is obviously '#{question["correct_answer"]}'"
             puts " "
